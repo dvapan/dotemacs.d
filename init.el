ed6e86087f500f4ef5c452b1a3dbf4a933bc7295
Danil Apanovich <dvapan@gmail.com>
2019-10-01 01:10:42 +0800

    Добавил yasnippet

--- init.el
+++ init.el
@@ -2,11 +2,17 @@
 (add-to-list 'load-path "~/.emacs.d/lib/ghub/")
 (add-to-list 'load-path "~/.emacs.d/lib/magit/")
 (add-to-list 'load-path "~/.emacs.d/lib/company/")
+(add-to-list 'load-path "~/.emacs.d/lib/yasnippet/")
+(add-to-list 'load-path "~/.emacs.d/lib/yasnippet-snippets/")
 
 (require 'better-defaults)
 (require 'magit)
 (require 'company)
 (add-hook 'after-init-hook 'global-company-mode)
+(require 'yasnippet)
+(yas-global-mode 1)
+
+(require 'yasnippet-snippets)          
 
 (require 'sr-speedbar)
 (global-set-key (kbd "<f12>") 'sr-speedbar-toggle)
