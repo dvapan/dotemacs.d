96074fc6edff4cdc616cc6e8e4b690a6de1aeccf
Danil Apanovich <dvapan@gmail.com>
2019-10-01 00:40:37 +0800

    Добавил сompany mode

--- init.el
+++ init.el
@@ -1,6 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/lib")
 (add-to-list 'load-path "~/.emacs.d/lib/ghub/")
 (add-to-list 'load-path "~/.emacs.d/lib/magit/")
+(add-to-list 'load-path "~/.emacs.d/lib/company/")
 
 (require 'better-defaults)
 (require 'magit)
+(require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+
