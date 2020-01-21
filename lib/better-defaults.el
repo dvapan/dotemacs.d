d5cbfef33f519135cfc4e2c90f341408e37edaf0
Danil Apanovich <dvapan@gmail.com>
2020-01-22 00:48:24 +0800

    projectile append

--- lib/better-defaults.el
+++ lib/better-defaults.el
@@ -54,6 +54,11 @@
          '(("CMakeLists\\.txt\\'" . cmake-mode))
          '(("\\.cmake\\'" . cmake-mode))
          auto-mode-alist))
+
+  (require 'projectile)
+  (projectile-mode +1)
+  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   
   (global-set-key (kbd "M-/") 'hippie-expand)
   (global-set-key (kbd "C-x C-b") 'ibuffer)
