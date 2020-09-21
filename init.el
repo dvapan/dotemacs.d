1859bc3b3502b8b4cbd654653573242a07c0a5fd
Danil Apanovich (MESI) <dvapan@gmail.com>
2020-09-21 14:28:15 +0800

    Добавлен evil-mode.

--- init.el
+++ init.el
@@ -4,6 +4,10 @@
 (add-to-list 'load-path "~/.emacs.d/lib/company/")
 (add-to-list 'load-path "~/.emacs.d/lib/yasnippet/")
 (add-to-list 'load-path "~/.emacs.d/lib/yasnippet-snippets/")
+(add-to-list 'load-path "~/.emacs.d/lib/evil/")
+
+(require 'evil)
+(evil-mode 1)
 
 (load "better-defaults.el")
 (require 'better-defaults)
