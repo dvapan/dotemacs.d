ff67166708a4bf45d96f7727cb54ff33db352a75
Danil Apanovich <dvapan@gmail.com>
2021-03-30 15:09:10 +0800

    reconfigure to use-package

--- init.el
+++ init.el
@@ -1,19 +1,72 @@
+;; Do not show the startup screen.
+(setq inhibit-startup-message t)
 
-(add-to-list 'load-path "~/.emacs.d/lib")
-(add-to-list 'load-path "~/.emacs.d/lib/ghub/")
-(add-to-list 'load-path "~/.emacs.d/lib/magit/")
-(add-to-list 'load-path "~/.emacs.d/lib/company/")
-(add-to-list 'load-path "~/.emacs.d/lib/yasnippet/")
-(add-to-list 'load-path "~/.emacs.d/lib/yasnippet-snippets/")
+;; Disable tool bar, menu bar, scroll bar.
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
 
-(require 'better-defaults)
-(require 'magit)
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+;; Highlight current line.
+(global-hl-line-mode t)
+
+;; Use `command` as `meta` in macOS.
+(setq mac-command-modifier 'meta)
+
+;; Do not use `init.el` for `custom-*` code - use `custom-file.el`.
+(setq custom-file "~/.emacs.d/custom-file.el")
+
+;; Assuming that the code in custom-file is execute before the code
+;; ahead of this line is not a safe assumption. So load this file
+;; proactively.
+(load-file custom-file)
+
+;; Require and initialize `package`.
+(require 'package)
+(package-initialize)
+
+;; Add `melpa` to `package-archives`.
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
+  (package-install 'use-package))
 
 
-(require 'yasnippet)
-(yas-global-mode 1)
+;; Additional packages and their configurations
 
-(require 'yasnippet-snippets)          
+(use-package spacemacs-theme
+  :ensure t
+  :defer t
+  :config
+  ;; Do not use a different background color for comments.
+  (setq spacemacs-theme-comment-bg nil)
+
+  ;; Comments should appear in italics.
+  (setq spacemacs-theme-comment-italic t)
+
+  :init
+  ;; Use the `spacemacs-dark` theme.
+  (load-theme 'spacemacs-dark))
+
+
+(use-package company
+  :ensure t
+  :defer t
+  ;; Navigate in completion minibuffer with `C-n` and `C-p`.
+  :bind (:map company-active-map
+         ("C-n" . company-select-next)
+         ("C-p" . company-select-previous))
+  :config
+  ;; Provide instant autocompletion.
+  (setq company-idle-delay 0.3)
+
+  :init
+  ;; Use company mode everywhere.
+  (global-company-mode t))
+
+(use-package magit
+  :ensure t
+  :defer t
+  :bind ("C-x g" . magit-status))
 
