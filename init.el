f96d420acfe3d13d471407c00d71ed2fa790fd07
Danil Apanovich <dvapan@gmail.com>
2024-10-15 12:28:47 +0600

    change font size

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 (global-set-key (kbd "C-=")   'text-scale-increase)
 (global-set-key (kbd "C--")   'text-scale-decrease)
 (global-set-key (kbd "<f9>")  'compile)
-(add-to-list 'default-frame-alist `(font . "Monospace-17"))
+(add-to-list 'default-frame-alist `(font . "Monospace-14"))
 
 (defun duplicate-line-upd ()
   "Duplicate current line"
