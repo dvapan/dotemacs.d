469560889c75c197cf6abaf883e746c5edf160e9
Danil Apanovich <dvapan@gmail.com>
2026-06-11 20:53:53 +0600

    Reduce default font size to 12

--- init.el
+++ init.el
@@ -43,7 +43,7 @@
 
 
 ;; Set default font size for all frames
-(add-to-list 'default-frame-alist '(font . "Monospace-14"))
+(add-to-list 'default-frame-alist '(font . "Monospace-12"))
 
 
 (defun duplicate-line-upd ()
